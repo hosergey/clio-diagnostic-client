a3558b7538c66a58a770e90b171fe50d8973f593
hosergey <118318665+hosergey@users.noreply.github.com>
2026-07-19 20:03:09 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ function App() {
     setResults(null);
     try {
       const res = await fetch(
-        "https://clio-diagnostic-backend-1.onrender.com",
+        "https://clio-diagnostic-backend-1.onrender.com/diagnose",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
